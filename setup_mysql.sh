a1cea62e4468811982cfa31e90b27f2472a9c0c4
BrianDMeuse <bdmeuse@brandeis.edu>
2017-05-18 13:17:12 -0400

    mySQL setup
    Place mysqldump.sql file in  ~/sql

--- setup_mysql.sh
+++ setup_mysql.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
 
-# Export MySQL root password before installing to prevent MySQL from prompting for a password during the installation process
-# http://stackoverflow.com/questions/18812293/vagrant-ssh-provisioning-mysql-password
-echo "mysql-server mysql-server/root_password password rootpwd" | debconf-set-selections
-echo "mysql-server mysql-server/root_password_again password rootpwd" | debconf-set-selections
+yum -y install mysql-server
 
-# Install mysql-server and create the archivesspace database
-# http://archivesspace.github.io/archivesspace/user/running-archivesspace-against-mysql/
-# https://gist.github.com/rrosiek/8190550
-apt-get -y install mysql-server
-mysql -uroot -prootpwd -e "create database archivesspace"
-mysql -uroot -prootpwd -e "grant all on archivesspace.* to 'as'@'localhost' identified by 'as123'"
+service mysqld start
 
-mysql -uroot -prootpwd -e "create database browse_pages"
-mysql -uroot -prootpwd -e "grant all on browse_pages.* to 'as'@'localhost' identified by 'as123'"
+# Don't do this!
+mysql -uroot -e "create database archivesspace"
+mysql -uroot -e "grant all on archivesspace.* to 'as'@'localhost' identified by 'as123'"
 
-mysql -uroot -prootpwd -e "create database aspacedev"
-mysql -uroot -prootpwd -e "grant all on aspacedev.* to 'as'@'localhost' identified by 'as123'"
+#yum -y install phpmyadmin
+
+#service httpd restart
+
+# load whatever mysqldump file we added
+mysql -uas -pas123 archivesspace < sql/mysqldump.sql
