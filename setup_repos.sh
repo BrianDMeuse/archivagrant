a39f5d4bb8639de69949b6e38a94fcc618430f29
BrianDMeuse <bdmeuse@brandeis.edu>
2017-05-24 14:24:56 -0400

    Added MySQL repo

--- setup_repos.sh
+++ setup_repos.sh
@@ -6,4 +6,8 @@ rpm -ivh http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.no
 # REMI
 rpm -ivh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
 
+# MySQL
+rpm -ivh https://dev.mysql.com/get/mysql57-community-release-el6-11.noarch.rpm
 
+# Install yum-config-manager
+yum install yum-utils
