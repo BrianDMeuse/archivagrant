7fa1cd435dc59157e6c5186427a8dea8d0af7e62
BrianDMeuse <bdmeuse@brandeis.edu>
2017-05-31 14:09:38 -0400

    Mimic production setup

--- setup_archivesspace.sh
+++ setup_archivesspace.sh
@@ -7,11 +7,21 @@ yum -y install git
 yum -y install curl
 
 cd /usr/local
+
 git clone https://github.com/BrianDMeuse/archivesspace.git
 
 chown -R vagrant:vagrant archivesspace
 
-cd archivesspace
+mkdir archivesspace-v1.4.1
+
+mv archivesspace archivesspace-v1.4.1/
+
+ln -s /usr/local/archivesspace-v1.4.1/archivesspace aspace
+
+cd aspace
+
+# tweak to point to localhost and not production
+sed -i 's/findingaids.brandeis.edu/localhost/' plugins/brandeis/public/views/site/index.html.erb
 
 ./archivesspace.sh start
 
