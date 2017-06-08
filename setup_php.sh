a18e5ad73fce0fce625e5d6aa75ff0b8f598c909
BrianDMeuse <bdmeuse@brandeis.edu>
2017-06-08 12:29:31 -0400

    Added phpMyAdmin to remotely access MySQL

--- setup_php.sh
+++ setup_php.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
-yum -y erase php
+yum -y install php
+yum -y install php-mysql
+yum -y install phpmyadmin
 
-yum -y install php71
+# Tweak conf so we can remotely access
+sed -i 's/Allow from 127.0.0.1/Allow from 10.0.2.2/' /etc/httpd/conf.d/phpMyAdmin.conf
+
+echo "Be sure to update /etc/phpMyAdmin/config.inc.php"
+
+service httpd restart
