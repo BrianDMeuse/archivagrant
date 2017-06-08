ae93932dce1b6a8fe44aef97ef4f8c9c541af2f9
BrianDMeuse <bdmeuse@brandeis.edu>
2017-06-08 12:28:45 -0400

    Adding mod_ssl

--- setup_http.sh
+++ setup_http.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 yum -y install httpd
+yum -y install mod_ssl
 
 # Change Listen port to avoid conflict with localhost
 sed -i 's/Listen 80/Listen 8000/' /etc/httpd/conf/httpd.conf
